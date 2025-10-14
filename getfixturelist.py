29d154fd37143ad1ba4e8e0085daf01602bb1ad8
Chris <cdb224@gmail.com>
2025-10-14 16:03:12 -0400

    getting ready to run 2018 mls fixtures

--- getfixturelist.py
+++ getfixturelist.py
@@ -92,7 +92,7 @@ print("")
 # Get what season you want the fixture list for
 print("Getting the season...")
 #season = int(input("Enter the season:  "))
-season = 2017
+season = 2018
 print(f"...season is {season}.")
 print("Storing the path to the api in a path variable...")
 path = f"/fixtures?team=1608&season={season}"
